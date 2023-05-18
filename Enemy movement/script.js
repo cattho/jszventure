ee45771df374f0dc704acd5e482ef9924b237bf7
Sebastian Giraldo <jusegiraldo76@gmail.com>
2023-05-18 09:20:42 -0500

    feat(Enemies): � add enemies animation

--- Enemy movement/script.js
+++ Enemy movement/script.js
@@ -11,24 +11,42 @@ enemyImage.src = "./Assests/enemy1.png";
 //create enemy class
 class Enemy {
   constructor() {
-    (this.positionX = Math.random() * CANVAS_WIDTH),
-      (this.positionY = Math.random() * CANVAS_HEIGHT),
-      (this.width = Math.random() * 50),
-      (this.height = Math.random() * 100),
-      (this.speed = Math.random() * 4 - 2);
+    this.positionX = Math.random() * CANVAS_WIDTH;
+    this.positionY = Math.random() * CANVAS_HEIGHT;
+    this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293; //muestra el ancho de la unidad enemiga,
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5; //este ancho es el tamaño real del enemigo
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0; //muestra la posicion en la que esta la imagen actualmente
   }
 
   //update method to update enemy position
   update() {
-    (this.positionX += this.speed), (this.positionY += this.speed);
+    this.positionX += this.speed;
+    this.positionY += this.speed;
+    //animacion de unidades enemigas
+    this.frame < 4 ? (this.frame = 0) : this.frame++;
   }
 
   //draw method render image enemy
   draw() {
-    ctx.strokeRect(this.positionX, this.positionY, this.width, this.height);
-    ctx.drawImage()
+    // ctx.strokeRect(this.positionX, this.positionY, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      this.frame * this.width,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.positionX,
+      this.positionY,
+      this.width,
+      this.height
+    );
   }
 }
+
+console.log(enemyArray);
 
 //create multiplies enemies
 for (let i = 0; i < numberOfEnemies; i++) {
